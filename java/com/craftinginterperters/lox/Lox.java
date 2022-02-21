de2515d4ca4d17957766fe5c434e9c8504d26ffd
stevexu <1161176156@qq.com>
2022-02-21 22:53:20 +0800

    feat: init

---
+++ java/com/craftinginterperters/lox/Lox.java
@@ -0,0 +1,8 @@
+package com.craftinginterpreters.lox;
+
+public class Lox {
+  public static void main(String[] args) {
+    System.out.println("Hello Java!");
+    System.out.println(args);
+  }
+}
