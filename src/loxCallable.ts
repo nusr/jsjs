c60aaca07514009d59a6d25365efa8adc5c0b71c
stevexu <1161176156@qq.com>
2022-05-26 23:15:05 +0800

    fix: file name

--- src/loxCallable.ts
+++ src/loxCallable.ts
@@ -2,7 +2,7 @@ import type { LiteralType } from './type';;
 import type Interpreter  from './interpreter';
 class LoxCallable {
   call(interpreter: Interpreter, argumentList: LiteralType[]): LiteralType {
-    return argumentList[0];
+    return argumentList[0] as LiteralType;
   }
   arity(): number {
     return 0;
