6912c0d942d2a3afa2570c534e1da8f2d898b2e0
Steve Xu <1161176156@qq.com>
2022-08-29 16:38:46 +0800

    Update delete.ts

--- scripts/delete.ts
+++ scripts/delete.ts
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-fs.rm(path.join(__dirname, '../lib'), { recursive: true }, (error) => {
+fs.rmdir(path.join(__dirname, '../lib'), { recursive: true }, (error) => {
   if (error) {
     console.log(error);
   }
