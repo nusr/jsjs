215efff645636aa3fb17c0e40c89b0caff4fcbb6
Steve Xu <1161176156@qq.com>
2022-08-31 16:24:06 +0800

    Update and rename post-install.sh to post-install.js

--- post-install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-rm -f scripts/*.js
-rm -f esbuild.js
-npx esbuild scripts/visitTest.ts --outfile=scripts/visitTest.js --format=cjs
-npx esbuild scripts/generateAST.ts --outfile=scripts/generateAST.js --format=cjs
-npx esbuild scripts/bin.ts --outfile=scripts/bin.js --format=cjs
-npx esbuild scripts/delete.ts --outfile=scripts/delete.js --format=cjs
-npx esbuild esbuild.ts --outfile=esbuild.js --format=cjs
